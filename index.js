bff0db18b092f9c17937f3a4bf0d71b797aea67f
Matthew Simone <103798764+MattSimone14@users.noreply.github.com>
2023-11-12 23:41:54 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -17,70 +17,70 @@ const PortfolioProjectsContainer = document.getElementById('PortfolioProjects');
 
 const PortfolioProjectsData = [
     {
-        imageSrc: 'assets/images/Hawk1.PNG',
+        imageSrc: 'assets/images/Hawk1.png',
         description: 'Front-End Project',
         websiteLink: 'https://mattsimone14.github.io/Hawk/',
         githubLink: 'https://github.com/MattSimone14/Hawk',
 
     },
     {
-        imageSrc: 'assets/images/mondrian1.PNG',
+        imageSrc: 'assets/images/mondrian1.png',
         description: 'CSS project using the Grid layout',
         websiteLink: 'https://mattsimone14.github.io/MondrianPainting/',
         githubLink: 'https://github.com/MattSimone14/MondrianPainting',
 
     },
     {
-        imageSrc: 'assets/images/pricing1.PNG',
+        imageSrc: 'assets/images/pricing1.png',
         description: 'CSS project using Flexbox',
         websiteLink: 'https://project1website.com',
         githubLink: 'https://github.com/user/project1',
 
     },
     {
-        imageSrc: 'assets/images/WDA1.PNG',
+        imageSrc: 'assets/images/WDA1.png',
         description: 'responsive website using media query',
         websiteLink: 'https://mattsimone14.github.io/WebDesignAgency/',
         githubLink: 'https://github.com/MattSimone14/WebDesignAgency',
 
     },
     {
-        imageSrc: 'assets/images/Flag1.PNG',
+        imageSrc: 'assets/images/Flag1.png',
         description: 'CSS project using a responsive square',
         websiteLink: 'https://mattsimone14.github.io/CSSFlag/',
         githubLink: 'https://github.com/MattSimone14/CSSFlag',
 
     },
     {
-        imageSrc: 'assets/images/Poster1.PNG',
+        imageSrc: 'assets/images/Poster1.png',
         description: 'CSS Properties Project',
         websiteLink: 'https://mattsimone14.github.io/MotivationalPoster/',
         githubLink: 'https://github.com/MattSimone14/MotivationalPoster',
 
     },
     {
-        imageSrc: 'assets/images/Vocab1.PNG',
+        imageSrc: 'assets/images/Vocab1.png',
         description: 'Learning the colors in German using CSS and HTML',
         websiteLink: 'https://mattsimone14.github.io/ColorVocab/',
         githubLink: 'https://github.com/MattSimone14/ColorVocab',
 
     },
     {
-        imageSrc: 'assets/images/Psite1.PNG',
+        imageSrc: 'assets/images/Psite1.png',
         description: 'Basic HTML portfolio',
         websiteLink: 'https://mattsimone14.github.io/html-portfolio/',
         githubLink: 'https://github.com/MattSimone14/html-portfolio',
 
     },
     {
-        imageSrc: 'assets/images/Bdayinvite1.PNG',
+        imageSrc: 'assets/images/Bdayinvite1.png',
         description: 'HTML website using anchor tags and image Elements',
         websiteLink: 'https://mattsimone14.github.io/BirthdayInvite/',
         githubLink: 'https://github.com/MattSimone14/BirthdayInvite',
 
     },
     {
-        imageSrc: 'assets/images/MovieRank1.PNG',
+        imageSrc: 'assets/images/MovieRank1.png',
         description: 'Simple HTML Website',
         websiteLink: 'https://mattsimone14.github.io/MovieRanking/',
         githubLink: 'https://github.com/MattSimone14/MovieRanking',
